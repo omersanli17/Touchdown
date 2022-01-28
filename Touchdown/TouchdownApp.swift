a66ae1057ddb14f4d432ae10e76cc5a85641a481
omer <sanli.omer@metu.edu.tr>
2022-01-28 20:52:40 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by omer sanli on 28.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
